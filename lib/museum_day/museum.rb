e12320bc6b979fe985acb8672010f1dddefad817
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-02 20:32:12 -0700

    Single responsibiliity principle. Moved all scraping code to scraper.rb

--- lib/museum_day/museum.rb
+++ lib/museum_day/museum.rb
@@ -21,38 +21,6 @@ class MuseumDay::Museum
     self.all[id-1]
   end
 
-  def address
-    @address ||= doc.css("p.address").text.strip
-  end
-
-  def phone_number
-    @phone_number ||= doc.at("i.fa-phone").next_sibling.text.strip
-  end
-
-  def website_url
-    @website_url ||= doc.css("i.fa-external-link + a").attribute("href").value
-  end
-
-  def social_urls
-    social_links = doc.css("div.contact a").collect { |link| link.attribute("href").value }
-
-    social_links.each do |link|
-      if link.include?("twitter")
-        self.twitter = link
-      elsif link.include?("facebook")
-        self.fb = link
-      end
-    end
-  end
-
-  def description
-    @description ||= doc.css("div.aux-info p").first.text
-  end
-
-  def doc
-    @doc ||= Nokogiri::HTML(open("https://www.smithsonianmag.com#{self.url}"))
-  end
-
   def self.clear_all
     @@all.clear
   end
